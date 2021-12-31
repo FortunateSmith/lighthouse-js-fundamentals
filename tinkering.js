4ca093ff4807a13a686b840fd2fff468e13e8ac4
Liam Smith <liam.s.smith@gmail.com>
2021-12-31 14:07:16 +0000

    Tinkering

---
+++ tinkering.js
@@ -0,0 +1,20 @@
+
+
+const price = 15.00
+const money = 20.00
+
+if(money >= price) {
+  console.log("Buy the hammer.");
+} else {
+  console.log("Don't buy the hammer.");
+}
+
+let weather = "sunny";
+
+if (weather === "snow") {
+  console.log("Wear a parka");
+} else if (weather === "rain") {
+  console.log("Wear a rain slicker");
+} else {
+  console.log("Wear what you have on");
+}
