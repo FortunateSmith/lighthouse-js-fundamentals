bea8601901474dd3b86e6447d5541d22b695da2b
Liam Smith <liam.s.smith@gmail.com>
2021-12-31 14:00:18 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,3 +1,4 @@
+
 const temperature = -370;
 
 if(temperature < 0){
@@ -9,3 +10,22 @@ else if(temperature < 15){
   console.log('Short sleeves are fine.');
 }
 console.log('Now you\'re ready to go outside!');
+
+const isCitizen = true;
+const age = 26;
+
+if(isCitizen===true && age>=18){
+  console.log('You are eligible to vote');
+}
+
+if(temperature<= -40 || temperature>= 40){
+  console.log('Maybe going outside isn\'t a great idea.');
+} 
+
+
+
+ 
+if(!raining){
+  console.log('Leave your umbrella at home');
+}
+
