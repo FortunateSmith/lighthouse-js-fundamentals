397b6b6f80ec99c301d2cf7b825c7b08bc48331e
Liam Smith <liam.s.smith@gmail.com>
2022-01-04 19:09:46 +0000

    flavour of ice cream array practise

---
+++ ice-cream.js
@@ -0,0 +1,7 @@
+const iceCreamFlavours = ['chocoalte', 'vanilla', 'cookies and cream', 'rocky road', 'strawberry'];
+console.log(iceCreamFlavours);
+
+const newFlavour = iceCreamFlavours.push('root beer');
+
+let first = iceCreamFlavours[0];
+
